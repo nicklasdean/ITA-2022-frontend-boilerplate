4e3b278f1aa5420d9d272aa0b65f7ce3a214accc
dean <nicklasfrederiksen@gmail.com>
2022-08-23 16:18:54 +0200

    first

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //et lille fetch eksempel
 fetch('http://localhost:3000/data').then((response) => response.json()).then((data) => {
     console.log(data);
-    document.querySelector('body').innerHTML = data;
+    document.querySelector('body').innerHTML = JSON.stringify(data);
 })
